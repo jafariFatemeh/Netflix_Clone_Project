72395b600470d36ded80d10c740c651a0c4a2d55
jafware <info@jafware.com>
2023-03-10 13:26:47 +0400

    Initial Commit

---
+++ Netflix_Clone_Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netflix_Clone_Project
+//
+//  Created by Parisa Jafari on 10/03/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
